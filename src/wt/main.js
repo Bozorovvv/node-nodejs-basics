38fc8426d16df50ee8595e9b187a9a07e997ea19
farhod <farhod.jonikulovich@gmail.com>
2024-10-04 00:14:57 +0500

    wt in progress

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,14 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const worker = new Worker("./worker.js");
+
+  const cpusCount = cpus();
+  console.log(cpusCount.length);
+  //   worker.postMessage(cpusCount.length);
+
+  //   worker.once("message", (a) => console.log(a));
 };
 
 await performCalculations();
